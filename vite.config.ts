338bd69980a1f54acea716ee98fd496305484292
Mary Nefo <nova@Marys-MacBook-Pro.local>
2026-03-09 11:57:17 +0100

    Set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Needed for GitHub Pages: https://supernovam.github.io/lesson-tracker/
+  base: '/lesson-tracker/',
   plugins: [react()],
   test: {
     globals: true,
